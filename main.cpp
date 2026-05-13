0de9238159732ac086aae195106e0f3675ba1d5b
JiaPoh1 <yeohjiapoh@gmail.com>
2026-05-13 23:01:43 +0800

    Update code

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 #include <iomanip>
 using namespace std;
 
-float transportation, food, utilities, medical_and_healthcare, personal_spending, entertainment;   // Fazly Reza, declare variables
 float sum_transportation, sum_food, sum_utilities, sum_medical_and_healthcare, sum_personal_spending, sum_entertainment, total_expenses;  // Fazly Reza, declare variables
-float transportation_percentage, food_percentage, utilities_percentage, medical_and_healthcare_percentage, personal_spending_percentage, entertainment_percentage;  // Fazly Reza, declare variables
-char answer = 'Y';
 
 double calculateTotalIncome() // Alifah, user input fixed income and variable. Then calculate the total income by adding fixed income with variable income
 {
 	double fixedIncome, variableIncome;
+
 	cout << "Enter your fixed income: ";
 	cin >> fixedIncome;
+
 	cout << "Enter your variable income: ";
 	cin >> variableIncome;
+
 	return fixedIncome + variableIncome;
 }
 
@@ -43,107 +43,104 @@ void compareSavings(double savingsGoal, double actualSavings) // Ain, this funct
 	}
 }
 
-float category_expenses_total()  // Fazly Reza, user input on expenses for each category and calculate total expenses by adding every expense type
+float inputCategoryExpenses(string categoryName) // Yeoh Jia Poh
 {
-	do {
-		cout << endl << "Enter your expenses on transportation: RM ";
-		cin >> transportation;
-		cout << "Enter Y to add more: ";
-		cin >> answer;
-		sum_transportation += transportation;
-	} while (answer == 'Y' || answer == 'y');
+	float expense;
+	float total = 0;
+	char answer;
 
 	do {
-		cout << endl << "Enter your expenses on food: RM ";
-		cin >> food;
+		cout << "\nEnter your expenses on " << categoryName << ": RM ";
+
+		cin >> expense;
+
+		total += expense;
+
 		cout << "Enter Y to add more: ";
 		cin >> answer;
-		sum_food += food;
 	} while (answer == 'Y' || answer == 'y');
 
-	do {
-		cout << endl << "Enter your expenses on utilities: RM ";
-		cin >> utilities;
-		cout << "Enter Y to add more: ";
-		cin >> answer;
-		sum_utilities += utilities;
-	} while (answer == 'Y' || answer == 'y');
-
-	do {
-		cout << endl << "Enter your expenses on medical and healthcare: RM ";
-		cin >> medical_and_healthcare;
-		cout << "Enter Y to add more: ";
-		cin >> answer;
-		sum_medical_and_healthcare += medical_and_healthcare;
-	} while (answer == 'Y' || answer == 'y');
-	do {
-		cout << endl << "Enter your expenses on personal spending: RM ";
-		cin >> personal_spending;
-		cout << "Enter Y to add more : ";
-		cin >> answer;
-		sum_personal_spending += personal_spending;
-	} while (answer == 'Y' || answer == 'y');
-
-	do {
-		cout << endl << "Enter your expenses on entertainment: RM ";
-		cin >> entertainment;
-		cout << "Enter Y to add more: ";
-		cin >> answer;
-		sum_entertainment += entertainment;
-	} while (answer == 'Y' || answer == 'y');
-	total_expenses = sum_transportation + sum_food + sum_utilities + sum_medical_and_healthcare + sum_personal_spending + sum_entertainment;
-	cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
-	cout << endl << "TRANSPORTATION:" << endl;
-	cout << "Your transportation total expenses are RM " << fixed << setprecision(2) << sum_transportation << endl;
-	cout << endl << "FOOD:" << endl;
-	cout << "Your food total expenses are RM " << sum_food << endl;
-	cout << endl << "UTILITIES:" << endl;
-	cout << "Your utilities total expenses are RM " << sum_utilities << endl;
-	cout << endl << "MEDICAL AND HEALTHCARE:" << endl;
-	cout << "Your medical and healthcare total expenses are RM " << sum_medical_and_healthcare << endl;
-	cout << endl << "PERSONAL SPENDING:" << endl;
-	cout << "Your personal spending total expenses are RM " << sum_personal_spending << endl;
-	cout << endl << "ENTERTAINMENT:" << endl;
-	cout << "Your entertainmet total expenses are RM " << sum_entertainment << endl;
-	cout << endl << "TOTAL EXPENSES:" << endl;
-	cout << "Your total expenses are RM " << total_expenses << endl;
-	cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
-	return sum_transportation, sum_food, sum_utilities, sum_medical_and_healthcare, sum_personal_spending, sum_entertainment, total_expenses;
+	return total;
 }
 
-float percentage_of_total_expenses(double &transportation_percentage, double &food_percentage, double &utilities_percentage, double &medical_and_healthcare_percentage  // Fazly Reza
+float category_expenses_total()  // Fazly Reza, user input on expenses for each category and calculate total expenses by adding every expense type
+{
+	sum_transportation = inputCategoryExpenses("transportation");
+	sum_food = inputCategoryExpenses("food");
+	sum_utilities = inputCategoryExpenses("utilities");
+	sum_medical_and_healthcare = inputCategoryExpenses("medical and healthcare");
+	sum_personal_spending = inputCategoryExpenses("personal spending");
+	sum_entertainment = inputCategoryExpenses("entertainment");
+
+	total_expenses = sum_transportation + sum_food + sum_utilities + sum_medical_and_healthcare + sum_personal_spending + sum_entertainment;
+
+	cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+
+	cout << endl << "TRANSPORTATION:" << endl;
+	cout << "Your transportation total expenses are RM " << fixed << setprecision(2) << sum_transportation << endl;
+
+	cout << endl << "FOOD:" << endl;
+	cout << "Your food total expenses are RM " << sum_food << endl;
+
+	cout << endl << "UTILITIES:" << endl;
+	cout << "Your utilities total expenses are RM " << sum_utilities << endl;
+
+	cout << endl << "MEDICAL AND HEALTHCARE:" << endl;
+	cout << "Your medical and healthcare total expenses are RM " << sum_medical_and_healthcare << endl;
+
+	cout << endl << "PERSONAL SPENDING:" << endl;
+	cout << "Your personal spending total expenses are RM " << sum_personal_spending << endl;
+
+	cout << endl << "ENTERTAINMENT:" << endl;
+	cout << "Your entertainmet total expenses are RM " << sum_entertainment << endl;
+
+	cout << endl << "TOTAL EXPENSES:" << endl;
+	cout << "Your total expenses are RM " << total_expenses << endl;
+
+	cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+	return total_expenses;
+}
+
+void percentage_of_total_expenses(double &transportation_percentage, double &food_percentage, double &utilities_percentage, double &medical_and_healthcare_percentage  // Fazly Reza
 	, double &personal_spending_percentage, double &entertainment_percentage)  // Fazly Reza, calculate percentage of each category expense
 {
 	cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+
 	transportation_percentage = sum_transportation / total_expenses * 100;
 	cout << endl << "TRANSPORTATION:" << endl;
 	cout << "Your transportation percentage of total expenses are " << transportation_percentage << "%" << endl;
+
 	food_percentage = sum_food / total_expenses * 100;
 	cout << endl << "FOOD:" << endl;
 	cout << "Your food percentage of total expenses are " << food_percentage << "%" << endl;
+
 	utilities_percentage = sum_utilities / total_expenses * 100;
 	cout << endl << "UTILITIES:" << endl;
 	cout << "Your utilities percentage of total expenses are " << utilities_percentage << "%" << endl;
+
 	medical_and_healthcare_percentage = sum_medical_and_healthcare / total_expenses * 100;
 	cout << endl << "MEDICAL AND HEALTHCARE:" << endl;
 	cout << "Your medical and healthcare percentage of total expenses are " << medical_and_healthcare_percentage << "%" << endl;
+
 	personal_spending_percentage = sum_personal_spending / total_expenses * 100;
 	cout << endl << "PERSONAL SPENDING:" << endl;
 	cout << "Your personal spending percentage of total expenses are " << personal_spending_percentage << "%" << endl;
+
 	entertainment_percentage = sum_entertainment / total_expenses * 100;
 	cout << endl << "ENTERTAINMENT:" << endl;
-	cout << "Your entertainmet percentage of total expenses are " << entertainment_percentage << "%" << endl;
+	cout << "Your entertainment percentage of total expenses are " << entertainment_percentage << "%" << endl;
+
 	cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
-	return transportation_percentage, food_percentage, utilities_percentage, medical_and_healthcare_percentage, personal_spending_percentage, entertainment_percentage;
 }
 
-void ProcessCategory(double &transportation_percentage , double &food_percentage, double &utilities_percentage, double &medical_and_healthcare_percentage, 
+void calculateTotalPercentage(double &transportation_percentage , double &food_percentage, double &utilities_percentage, double &medical_and_healthcare_percentage,
 	double &personal_spending_percentage, double &entertainment_percentage, double &TotalPercentage)  /* Yeoh Jia Poh, calculate total percentage
 	by adding all percentage for each category*/
 {
 	TotalPercentage = transportation_percentage + food_percentage + utilities_percentage + medical_and_healthcare_percentage 
 		+ personal_spending_percentage + entertainment_percentage;
 }
+
 void CalculateBudgetAllocation(double &AvailableBudget, double TotalIncome, double TotalExpenses, double &TotalBudgetAllCategories, double TotalPercentage)  /* Yeoh Jia Poh
 calculate available budget by subtracting total income with total expenses, assign totalBudgetAllCategories with the value in TotalPercentage */
 {
@@ -153,66 +150,42 @@ calculate available budget by subtracting total income with total expenses, assi
 	cout << "Total percentage of all categories: " << TotalBudgetAllCategories << "%" << endl;
 }
 
-void ForCategoryBudgetTransportation(double &CategoryBudgetShare, double CategoryPriorityBudget, double TotalBudgetAllCategories, double AvailableBudget ) /* Yeoh Jia Poh
-Calculate the budget for transportation*/
+void calculateCategoryBudget(string categoryName, double& categoryBudget, double categoryPercentage, double totalPercentage, double availableBudget) /* Yeoh Jia Poh
+Calculate budget for all category*/
 {
-	CategoryBudgetShare = (CategoryPriorityBudget / TotalBudgetAllCategories) * AvailableBudget;
-	cout << "Category budget transportation: RM" << CategoryBudgetShare << endl;
+	categoryBudget = (categoryPercentage / totalPercentage) * availableBudget;
+	cout << categoryName << " budget: RM " << categoryBudget << endl;
 }
 
-void ForCategoryBudgetFood(double& CategoryBudgetShare, double CategoryPriorityBudget, double TotalBudgetAllCategories, double AvailableBudget) /* Yeoh Jia Poh
-Calculate the budget for food*/
-{
-	CategoryBudgetShare = (CategoryPriorityBudget / TotalBudgetAllCategories) * AvailableBudget;
-	cout << "Category budget food: RM" << CategoryBudgetShare << endl;
-}
-void ForCategoryBudgetUtilities(double& CategoryBudgetShare, double CategoryPriorityBudget, double TotalBudgetAllCategories, double AvailableBudget) /* Yeoh Jia Poh
-Calculate the budget for utilities*/
-{
-	CategoryBudgetShare = (CategoryPriorityBudget / TotalBudgetAllCategories) * AvailableBudget;
-	cout << "Category budget utilities: RM" << CategoryBudgetShare << endl;
-}
-void ForCategoryBudgetMedicalAndHealthcare(double& CategoryBudgetShare, double CategoryPriorityBudget, double TotalBudgetAllCategories, double AvailableBudget) /* Yeoh Jia Poh
-Calculate the budget for medical and healthcare*/
-{
-	CategoryBudgetShare = (CategoryPriorityBudget / TotalBudgetAllCategories) * AvailableBudget;
-	cout << "Category budget medical and healthcare: RM" << CategoryBudgetShare << endl;
-}
-void ForCategoryBudgetPersonalSpending(double& CategoryBudgetShare, double CategoryPriorityBudget, double TotalBudgetAllCategories, double AvailableBudget) /* Yeoh Jia Poh
-Calculate the budget for personal spending*/
-{
-	CategoryBudgetShare = (CategoryPriorityBudget / TotalBudgetAllCategories) * AvailableBudget;
-	cout << "Category budget personal spending: RM" << CategoryBudgetShare << endl;
-}
-void ForCategoryBudgetEntertainment(double& CategoryBudgetShare, double CategoryPriorityBudget, double TotalBudgetAllCategories, double AvailableBudget) /* Yeoh Jia Poh
-Calculate the budget for entertainment*/
-{
-	CategoryBudgetShare = (CategoryPriorityBudget / TotalBudgetAllCategories) * AvailableBudget;
-	cout << "Category budget entertainment: RM" << CategoryBudgetShare << endl;
-}
 int main()
 {
-	double totalIncome = calculateTotalIncome(); // Alifah
-	int fixedCount; 
-	double totalFixedExpenses = calculateTotalExpenses("fixed", fixedCount); // Alifah
+	int fixedCount;
 	int variableCount;
+
+	double totalIncome = calculateTotalIncome(); // Alifah
+	
+	double totalFixedExpenses = calculateTotalExpenses("fixed", fixedCount); // Alifah
 	double totalVariableExpenses = calculateTotalExpenses("variable", variableCount); // Alifah
+
 	double totalExpenses = totalFixedExpenses + totalVariableExpenses;
 	double actualSavings = totalIncome - totalExpenses;
 	double savingsGoal;
+
 	double AvailableBudget, TotalBudgetAllCategories;
-	double totalPriorityTotal,  Available_budget;
-	double CategoryBudgetA, CategoryBudgetB, CategoryBudgetC, CategoryBudgetD, CategoryBudgetE, CategoryBudgetF;
+	double totalPercentage;
+	double CategoryBudgetTransportation, CategoryBudgetFood, CategoryBudgetUtilities, CategoryBudgetMedicalAndHealthCare, CategoryBudgetPersonalSpending, CategoryBudgetEntertainment;
 	double transportation_percentage, food_percentage, utilities_percentage, medical_and_healthcare_percentage, personal_spending_percentage, entertainment_percentage;
-	string transportation, food, utilities, medical_and_healthcare, personal_spending, entertainment;
+
+
 	cout << "Enter your savings goal: ";
 	cin >> savingsGoal;
 	cout << "\n--- Results ---\n";
-	cout << "Total Income:RM" << totalIncome << endl;
+	cout << "Total Income:RM " << totalIncome << endl;
 	cout << "Total Fixed Expenses:RM " << totalFixedExpenses << endl;
 	cout << "Total Variable Expenses:RM " << totalVariableExpenses << endl;
 	cout << "Total Expenses:RM " << totalExpenses << endl;
 	cout << "Actual Savings:RM " << actualSavings << endl;
+
 	compareSavings(savingsGoal, actualSavings); // Ain
 	char answer2 = 'Y';
 	cout << "This program will generate the expense report." << endl;
@@ -220,16 +193,19 @@ int main()
 	category_expenses_total(); // Fazly Reza
 	cout << endl << "Enter Y to see the the percentage of total expenses on each category: ";
 	cin >> answer2;
+
 	if (answer2 == 'Y' || answer2 == 'y')
 	percentage_of_total_expenses(transportation_percentage, food_percentage, utilities_percentage, medical_and_healthcare_percentage, 
 		personal_spending_percentage, entertainment_percentage);  // Fazly Reza
-	ProcessCategory(transportation_percentage, food_percentage, utilities_percentage, medical_and_healthcare_percentage,
-		personal_spending_percentage, entertainment_percentage, totalPriorityTotal);  // Yeoh Jia Poh
-	CalculateBudgetAllocation(AvailableBudget, totalIncome, totalExpenses, TotalBudgetAllCategories, totalPriorityTotal); // Yeoh Jia Poh
-	ForCategoryBudgetTransportation(CategoryBudgetA, transportation_percentage, totalPriorityTotal, AvailableBudget); // Yeoh Jia Poh
-	ForCategoryBudgetFood(CategoryBudgetB, food_percentage, totalPriorityTotal, AvailableBudget); // Yeoh Jia Poh
-	ForCategoryBudgetUtilities(CategoryBudgetC, utilities_percentage, totalPriorityTotal, AvailableBudget); // Yeoh Jia Poh
-	ForCategoryBudgetMedicalAndHealthcare(CategoryBudgetD, medical_and_healthcare_percentage, totalPriorityTotal, AvailableBudget); // Yeoh Jia Poh
-	ForCategoryBudgetPersonalSpending(CategoryBudgetE, personal_spending_percentage, totalPriorityTotal, AvailableBudget); // Yeoh Jia Poh
-	ForCategoryBudgetEntertainment(CategoryBudgetF, entertainment_percentage, totalPriorityTotal, AvailableBudget); // Yeoh Jia Poh
+
+	calculateTotalPercentage(transportation_percentage, food_percentage, utilities_percentage, medical_and_healthcare_percentage,
+		personal_spending_percentage, entertainment_percentage, totalPercentage);  // Yeoh Jia Poh
+
+	CalculateBudgetAllocation(AvailableBudget, totalIncome, totalExpenses, TotalBudgetAllCategories, totalPercentage); // Yeoh Jia Poh
+	calculateCategoryBudget("Transportation", CategoryBudgetTransportation, transportation_percentage, totalPercentage, AvailableBudget); // Yeoh Jia Poh
+	calculateCategoryBudget("Food", CategoryBudgetFood, food_percentage, totalPercentage, AvailableBudget); // Yeoh Jia Poh
+	calculateCategoryBudget("Utilities", CategoryBudgetUtilities, utilities_percentage, totalPercentage, AvailableBudget); // Yeoh Jia Poh
+	calculateCategoryBudget("Medical and Healthcare", CategoryBudgetMedicalAndHealthCare, medical_and_healthcare_percentage, totalPercentage, AvailableBudget); // Yeoh Jia Poh
+	calculateCategoryBudget("Personal Spending", CategoryBudgetPersonalSpending, personal_spending_percentage, totalPercentage, AvailableBudget); // Yeoh Jia Poh
+	calculateCategoryBudget("Entertainment", CategoryBudgetEntertainment, entertainment_percentage, totalPercentage, AvailableBudget); // Yeoh Jia Poh
 }
